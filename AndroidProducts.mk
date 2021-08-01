30e6af6d06adbda520835bec7c006b7e388e3def
RakeshBatra <raakesh.batra@rediffmail.com>
2021-08-01 00:45:51 -0700

    ColtOS Bringup
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_miatoll.mk
+    $(LOCAL_DIR)/colt_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_miatoll-user \
-    arrow_miatoll-userdebug \
-    arrow_miatoll-eng
+    colt_miatoll-user \
+    colt_miatoll-userdebug \
+    colt_miatoll-eng
